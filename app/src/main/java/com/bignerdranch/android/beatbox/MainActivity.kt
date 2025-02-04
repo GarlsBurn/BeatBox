1124bc2db937ae6674c0974335c97608ade060a7
Ilya Belikov <belikovilya0@gmail.com>
2025-02-05 01:34:05 +0300

    Настройка модели представления

--- app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -32,7 +32,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private inner class SoundHolder(private val binding: ListItemSoundBinding):
-            RecyclerView.ViewHolder(binding.root){}
+            RecyclerView.ViewHolder(binding.root){
+
+                init {
+                    binding.viewModel = SoundViewModel()
+                }
+
+                fun bind(sound: Sound){
+                    binding.apply {
+                        viewModel?.sound = sound
+                        executePendingBindings()
+                    }
+                }
+            }
 
     private inner class SoundAdapter(private val sounds: List<Sound>):
             RecyclerView.Adapter<SoundHolder>(){
@@ -47,6 +59,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+            val sound = sounds[position]
+            holder.bind(sound)
         }
 
         override fun getItemCount(): Int = sounds.size
